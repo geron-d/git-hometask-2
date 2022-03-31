27586e87252be90a2a89ef6bccdf09ca3afbf7e4
geron_d <geron_d@mail.ru>
2022-03-31 18:37:09 +0300

    make methods for print square

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -14,25 +14,41 @@ import by.it.academy.square.triangle.TriangleSquareImplementation;
 
 public class App {
     public static void main(String[] args) {
+        printTriangleSquare();
+
+
+        printIsoscelesTriangleSquare();
+
+        printRightTriangleSquare();
+
+        printRectangleSquare();
+    }
+
+    private static void printRectangleSquare() {
+        RectangleSquare rectangleSquare = new RectangleSquareImplementation();
+        System.out.println("squareThroughWidthAndLength : " + rectangleSquare.squareThroughWidthAndLength(5, 5));
+        System.out.println("squareThroughQuadrilateralFormula : " + rectangleSquare.squareThroughQuadrilateralFormula(7.071, 90));
+        System.out.println("squareThroughSideAndDiagonal : " + rectangleSquare.squareThroughSideAndDiagonal(5, 7.071));
+    }
+
+    private static void printRightTriangleSquare() {
+        RightTriangleSquare rightTriangleSquare = new RightTriangleSquareImplementation();
+        System.out.println("squareThroughTwoLegs : " + rightTriangleSquare.squareThroughTwoLegs(5, 5));
+        System.out.println("squareThroughHypotenuseAndSharpAngle : " + rightTriangleSquare.squareThroughHypotenuseAndSharpAngle(7, 45));
+        System.out.println("squareThroughLegAndAdjacentAngles : " + rightTriangleSquare.squareThroughLegAndAdjacentAngles(5, 45));
+    }
+
+    private static void printIsoscelesTriangleSquare() {
+        IsoscelesTriangleSquare isoscelesTriangleSquare = new IsoscelesTriangleSquareImplementation();
+        System.out.println("squareThroughBaseAndHeight : " + isoscelesTriangleSquare.squareThroughBaseAndHeight(7.071, 3.536));
+        System.out.println("squareThroughSidesAndAngleBetweenThem : " + isoscelesTriangleSquare.squareThroughSidesAndAngleBetweenThem(5, 90));
+    }
+
+    private static void printTriangleSquare() {
         TriangleSquare triangleSquare = new TriangleSquareImplementation();
         System.out.println("squareThroughBaseAndHeight : " + triangleSquare.squareThroughBaseAndHeight(5, 5));
         System.out.println("squareThroughTwoSidesAndAngleBetweenThem : " + triangleSquare.squareThroughTwoSidesAndAngleBetweenThem(5, 5, 90));
         System.out.println("squareThroughSidesAndTwoAdjacentAngles : " + triangleSquare.squareThroughSidesAndTwoAdjacentAngles(5, 90, 45));
         System.out.println();
-
-
-        IsoscelesTriangleSquare isoscelesTriangleSquare = new IsoscelesTriangleSquareImplementation();
-        System.out.println("squareThroughBaseAndHeight : " + isoscelesTriangleSquare.squareThroughBaseAndHeight(7.071, 3.536));
-        System.out.println("squareThroughSidesAndAngleBetweenThem : " + isoscelesTriangleSquare.squareThroughSidesAndAngleBetweenThem(5, 90));
-
-        RightTriangleSquare rightTriangleSquare = new RightTriangleSquareImplementation();
-        System.out.println("squareThroughTwoLegs : " + rightTriangleSquare.squareThroughTwoLegs(5, 5));
-        System.out.println("squareThroughHypotenuseAndSharpAngle : " + rightTriangleSquare.squareThroughHypotenuseAndSharpAngle(7, 45));
-        System.out.println("squareThroughLegAndAdjacentAngles : " + rightTriangleSquare.squareThroughLegAndAdjacentAngles(5, 45));
-
-        RectangleSquare rectangleSquare = new RectangleSquareImplementation();
-        System.out.println("squareThroughWidthAndLength : " + rectangleSquare.squareThroughWidthAndLength(5, 5));
-        System.out.println("squareThroughQuadrilateralFormula : " + rectangleSquare.squareThroughQuadrilateralFormula(7.071, 90));
-        System.out.println("squareThroughSideAndDiagonal : " + rectangleSquare.squareThroughSideAndDiagonal(5, 7.071));
     }
 }
